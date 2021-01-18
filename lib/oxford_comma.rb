f9692287f6b0e9373fffaee8cff627ff9e2cc0ae
Tristan Gibbs <gtristan89@gmail.com>
2021-01-17 18:52:47 -0700

    commit tree 84b934fad82233cf911925e4b6af2ad77eead622

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,8 @@ def oxford_comma(array)
         array[0]
     elsif  array.length == 2
         array.join(" and ")
-    elsif array.length == 3
-        array[0..1].join(", ") + ", and " + array[-1]
+    else
+        x = array.length - 2
+        array[0..x].join(", ") + ", and " + array[-1]
     end
 end
